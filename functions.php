5b1f742a0405befb6ceb0d9ed233f34c288531cd
Anderson Nascimento <theandersonn@gmail.com>
2015-06-28 01:52:56 -0300

    Insere add_theme_support

--- functions.php
+++ functions.php
@@ -11,9 +11,14 @@ add_action('wp_enqueue_scripts', 'btwp_enqueue_scripts');
 function btwp_enqueue_scripts() {
 	// Carrega os estilos do tema
 	wp_enqueue_style('site-css', get_stylesheet_directory_uri() . '/assets/css/site.css');
+	wp_enqueue_style('reset-css', get_stylesheet_directory_uri() . '/assets/css/reset.css');
 	wp_enqueue_script( 'scripts-js', get_stylesheet_directory_uri() . '/assets/js/scripts.js');
 	// Carrega jQuery nativo do WordPress
 	wp_enqueue_script('jquery');
 }
+
+// Habilitando imagens destacadas
+add_image_size('post-thumb', 300, 225, true);
+add_theme_support('post-thumbnails');
 
 ?>
---
+++ tag.php
@@ -0,0 +1,7 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<!-- post -->
+<?php endwhile; ?>
+<!-- post navigation -->
+<?php else: ?>
+<!-- no posts found -->
+<?php endif; ?>
