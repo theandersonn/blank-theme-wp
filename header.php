98053b992b7fbca328498fe9a0fd99efb4de411a
Anderson Nascimento <theandersonn@gmail.com>
2015-07-13 21:49:19 -0300

    Insere atributos no head

--- header.php
+++ header.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html <?php language_attributes(); ?>>
     <head>
-        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0;">
-
-        <!-- FAVICON -->
-        <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/assets/images/favicon.ico" type="image/x-icon"/>
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
         <?php wp_head(); ?>
-   </head>
+    </head>
 <body>
 
 	<!-- TOPO SITE -->
 	<header>
-		
+
 	</header>
