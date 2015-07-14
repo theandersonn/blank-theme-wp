3d233afaca116effc7d925e7d26227fab4592e68
Anderson Nascimento <theandersonn@gmail.com>
2015-07-13 22:30:27 -0300

    Remove comentários

--- footer.php
+++ footer.php
@@ -1,18 +1,8 @@
-<?php
-/**
- * Esta página exibe o rodapé do site.
- *
- * Contém todo o conteúdo do rodapé.
- *
- * @package Blank Theme WP
- */
-?>
+<!-- RODAPÉ SITE -->
+<footer>
 
-	<!-- RODAPÉ SITE -->
-	<footer>
-		
-	</footer>
-	
-<?php wp_footer(); ?>	
+</footer>
+
+<?php wp_footer(); ?>
 </body>
 </html>
