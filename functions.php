6e1305242aa5f61680bc8ecf974dc7db54d60876
Anderson Nascimento <theandersonn@gmail.com>
2015-07-13 21:58:45 -0300

    Atualiza wp_enqueue_scripts com chamadas Owl Carousel

--- functions.php
+++ functions.php
@@ -14,14 +14,22 @@ add_action('wp_head', 'site_favicon');
 add_action('wp_enqueue_scripts', 'btwp_enqueue_scripts');
 
 function btwp_enqueue_scripts() {
+
 	// Carrega os estilos do tema
 	wp_enqueue_style('site-css', get_stylesheet_directory_uri() . '/assets/css/site.css');
 	wp_enqueue_style('reset-css', get_stylesheet_directory_uri() . '/assets/css/reset.css');
 	wp_enqueue_style('media-queries-css', get_stylesheet_directory_uri() . '/assets/css/media-queries.css');
-	// Carrega os scripts js do tema
-	wp_enqueue_script( 'scripts-js', get_stylesheet_directory_uri() . '/assets/js/scripts.js');
+
+	// Carrega os estilos do Owl Carousel
+	wp_enqueue_style('owl-carousel-css', get_stylesheet_directory_uri() . '/assets/css/owl-carousel/owl.carousel.css');
+	wp_enqueue_style('owl-theme-css', get_stylesheet_directory_uri() . '/assets/css/owl-carousel/owl.theme.css');
+
 	// Carrega jQuery nativo do WordPress
 	wp_enqueue_script('jquery');
+
+	// Carrega os scripts do Owl Carousel
+	wp_enqueue_script('owl-carousel-js', get_stylesheet_directory_uri() . '/assets/js/owl.carousel.min.js');
+	wp_enqueue_script('scripts-js', get_stylesheet_directory_uri() . '/assets/js/scripts.js');
 }
 
 /*--------------------------------------------------------------
