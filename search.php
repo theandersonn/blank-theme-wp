a31fcea5f19f30d455e0b77f36a2162ae67152a4
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-07-17 14:14:09 -0300

    Insere search e searchform.php

---
+++ search.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<h2 class="tt-noticias-do-setor">Resultado da busca pelo termo '<?php echo esc_html ( get_query_var('s') ); ?>'</h2>
+
+<!-- aqui loop de posts -->
+
+<!-- aqui painação -->
+
+<?php get_footer(); ?>
---
+++ searchform.php
@@ -0,0 +1,5 @@
+<div class="search">
+    <form method="get" action="<?php echo home_url(); ?>">
+        <input type="text" name="s" class="box-busca" placeholder="O que você procura?">
+    </form>
+</div>
