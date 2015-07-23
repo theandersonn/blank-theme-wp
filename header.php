59216be7183473a5f0de82f8b112833db83e1e06
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-07-23 13:56:18 -0300

    Insere Header > HTML5 Shiv v3.7.0

--- header.php
+++ header.php
@@ -7,9 +7,8 @@
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+    	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/html5.js"></script>
+    	<![endif]-->
         <?php wp_head(); ?>
     </head>
 <body>
