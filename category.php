cecba40e75f883eee57b5cb3386aeeaa322236fe
Anderson Nascimento <theandersonn@gmail.com>
2015-07-29 01:41:02 -0300

    Insere Loop em category.php

--- category.php
+++ category.php
@@ -1,5 +1,23 @@
 <?php get_header(); ?>
+
+<section>
+
+    <h2>Categoria: <?php single_cat_title(); ?></h2>
+
+    <!-- CONTEÚDO PADRÃO DAS PÁGINAS ESTÁTICAS -->
+    <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+
+    <?php endwhile; ?>
+
+    <?php else : ?>
+    	<p>Nenhum conteúdo encontrado.</p>
+    <?php endif; ?>
+
     <!-- obter paginação -->
     <?php wp_pagination();?>
+</section>
 
 <?php get_footer(); ?>
