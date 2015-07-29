8571ed8d0df44699688cbc25531c7a7bc0bbbd8a
Anderson Nascimento <theandersonn@gmail.com>
2015-07-29 01:12:52 -0300

    Insere Template de Comentários no Single

--- single.php
+++ single.php
@@ -1,10 +1,19 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : the_post(); ?>
+<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-    <h1><?php the_title(); ?></h1>
-	<p><?php the_content(); ?></p>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
 
+<?php endwhile; ?>
+
+<?php else : ?>
+    <p>Nenhum conteúdo encontrado.</p>
 <?php endif; ?>
+
+<!-- COMENTÁRIOS -->
+<div class="containner_comments">
+    <?php comments_template(); ?>
+</div>
 
 <?php get_footer(); ?>
