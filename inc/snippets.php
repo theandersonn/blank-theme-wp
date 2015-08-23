2c06414da4674d2184f30b393861ae0a57816a7e
Anderson <theandersonn@gmail.com>
2015-08-23 11:57:28 -0300

    Insere Colinha > wp-config

---
+++ inc/snippets.php
@@ -0,0 +1,15 @@
+<?php
+// ===== COLINHA WP-CONFIG ===== //
+
+// Desativando o sistema de revisões
+define('WP_POST_REVISIONS', false );
+
+// Limitando o número de revisões de um post
+define('WP_POST_REVISIONS', 3);
+
+// Remove BRs do contact form //
+define ('WPCF7_AUTOP', false );
+
+// ===== COLINHA LOOP ===== //
+
+?>
