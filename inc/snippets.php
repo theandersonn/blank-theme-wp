b6a21f7798e3ae2482a2fc475efdf0fdc63fadce
Anderson <theandersonn@gmail.com>
2015-08-23 12:10:25 -0300

    Insere Estrutura padrão do Loop WordPress

--- inc/snippets.php
+++ inc/snippets.php
@@ -7,9 +7,16 @@ define('WP_POST_REVISIONS', false );
 // Limitando o número de revisões de um post
 define('WP_POST_REVISIONS', 3);
 
-// Remove BRs do contact form //
+// Remove BRs do contact form
 define ('WPCF7_AUTOP', false );
 
 // ===== COLINHA LOOP ===== //
 
-?>
+// Estrutura padrão do Loop WordPress
+<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+	<h2><?php the_title(); ?></h2>
+	<?php the_content(); ?>
+
+<?php endwhile; endif;
+wp_reset_postdata(); ?>
