bd1e9bb7a6e9844a53b4a9d71fd1fc4bcd35bc63
Anderson <theandersonn@gmail.com>
2015-08-23 12:14:03 -0300

    Insere Loop customizado com WP_Query

--- inc/snippets.php
+++ inc/snippets.php
@@ -18,5 +18,18 @@ define ('WPCF7_AUTOP', false );
 	<h2><?php the_title(); ?></h2>
 	<?php the_content(); ?>
 
-<?php endwhile; endif;
-wp_reset_postdata(); ?>
+<?php endwhile; endif; wp_reset_postdata(); ?>
+
+// Loop customizado com WP_Query
+$nome-slug = new WP_Query(
+	array(
+		'posts_per_page' => '3',
+		'category_name' => 'nome-slug'
+	)
+);
+
+<?php if( $nome-slug->have_posts() ) : while( $nome-slug->have_posts() ) : $nome-slug->the_post(); ?>
+
+// Aqui Template tags
+
+<?php endwhile; endif; wp_reset_postdata(); ?>
