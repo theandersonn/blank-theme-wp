7857dd105421cbad89aa528754ee28d94fdad0a7
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-09-15 15:48:08 -0300

    Remove loop-main.php

--- partials/loop-main.php
+++
@@ -1,9 +0,0 @@
-<!-- START LOOP MAIN -->
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-	<h2><?php the_title(); ?></h2>
-	<p><?php the_content(); ?></p>
-<?php endwhile; ?>
-
-<?php else : ?>
-	<p>Não temos posts publicados</p>
-<?php endif; ?>
