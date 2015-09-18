657ead460e9aa452aeb700e21d6a0451bd0b0772
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-09-18 10:19:07 -0300

    Insere Actions p/ Remover lixo do Head

--- functions.php
+++ functions.php
@@ -1,7 +1,18 @@
 <?php
 
- //	Bloqueia a exibição da versão do WordPress
- remove_action( 'wp_head', 'wp_generator' );
+//	Remove a exibição da versão do WordPress no Head
+remove_action( 'wp_head', 'wp_generator' );
+
+// Remove lixo do Head
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 
  // Insere Favicon
 require get_template_directory() . '/inc/adding-favicon.php';
