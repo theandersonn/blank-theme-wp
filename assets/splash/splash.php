731e64e64f81e743c9b5189593e3c5f13343d3ef
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-10-26 18:02:51 -0200

    Insere pasta slash com img e splash.php

---
+++ assets/splash/splash.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Aladdin-PMI</title>
+	<style>
+	body{
+		background: #50c3c6
+	}
+	.container-splash{
+		max-width: 741px;
+		margin: 0 auto;
+	}
+	.img-splash{
+		width: 100%;
+		display: block;
+	}
+
+	</style>
+</head>
+<body>
+	<div class="container-splash">
+		<img class="img-splash" src="<?php bloginfo('template_url')?>/splash/site-construcao.png">
+	</div>
+</body>
+</html>
