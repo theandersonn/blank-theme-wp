4a30c393b89ed013d836d0eee9a052ef52829e5d
Anderson <theandersonn@gmail.com>
2015-10-31 20:39:43 -0200

    Ajusta modo manutenção > splash

--- inc/active-maintenace-mode.php
+++ inc/active-maintenace-mode.php
@@ -3,7 +3,7 @@
 //modo manutenção
 function maintenace_mode() {
   if ( !current_user_can( 'edit_themes' ) || !is_user_logged_in() ) {
-  	get_template_part('assets/splash/splash');
+  	get_template_part('splash/splash');
     die();
   }
 }
--- splash/splash.php
+++ splash/splash.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Aladdin-PMI</title>
+	<title>Site em Manutenção</title>
 	<style>
 	body{
 		background: #50c3c6
@@ -21,7 +21,8 @@
 </head>
 <body>
 	<div class="container-splash">
-		<img class="img-splash" src="<?php bloginfo('template_url')?>/splash/site-construcao.png">
+		<p>Site em Manutenção</p>
+		<!-- <img class="img-splash" src="<?php //bloginfo('template_url')?>/splash/site-construcao.png"> -->
 	</div>
 </body>
 </html>
