8c96513bb2fec7e3e03e3327e2dc734e6b97bd42
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-11-09 13:59:46 -0200

    Atualiza slider owl com chamada css

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 
 <!-- CONTEÚDO SITE -->
 <main>
-	MAIN
+	<!-- SLIDER -->
+	<ul class="owl-carousel">
+	    <?php
+	        $slider = new WP_query(
+	            array(
+	                'post_type' => 'slider',
+	                'term' => 'slider-home', // Crie a Categoria Slider Home
+	                'posts_per_page' => -1
+	            )
+	        );
+	            if( $slider->have_posts() ) : while( $slider->have_posts() ) : $slider->the_post();
+	     ?>
+	                <li class="item">
+	                    <?php the_post_thumbnail(); ?>
+	                </li>
+	            <?php endwhile; endif; wp_reset_postdata(); ?>
+	</ul>
 </main>
 
 <?php get_footer(); ?>
