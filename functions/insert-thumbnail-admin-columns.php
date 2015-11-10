e6a6b5ff8a25206ed6f0feb4e04d30ae5d9c5ddd
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-11-10 14:05:34 -0200

    Insere funçã exibição de IDs de páginas e posts

--- functions/insert-thumbnail-admin-columns.php
+++ functions/insert-thumbnail-admin-columns.php
@@ -15,3 +15,27 @@ function posts_custom_columns($column_name, $id){
 		echo the_post_thumbnail( array(150,100) );
     }
 }
+
+/*--------------------------------------------------------------
+	CRIA COLUNA PARA IDs
+--------------------------------------------------------------*/
+add_filter('manage_posts_columns', 'posts_columns_id', 5);
+add_filter('manage_pages_columns', 'posts_columns_id', 5);
+
+function posts_columns_id($defaults){
+	$defaults['btwp_post_id'] = __('ID');
+	return $defaults;
+}
+
+
+/*--------------------------------------------------------------
+	INSERE IDs DE POSTAGENS E PÁGINAS
+--------------------------------------------------------------*/
+add_action('manage_posts_custom_column', 'posts_custom_id_columns', 5, 2);
+add_action('manage_pages_custom_column', 'posts_custom_id_columns', 5, 2);
+
+function posts_custom_id_columns($column_name, $id){
+	if($column_name === 'btwp_post_id'){
+		echo $id;
+    }
+}
