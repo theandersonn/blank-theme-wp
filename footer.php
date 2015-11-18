99df7dfec0216d95c976cf2674f6d3a26047664b
Anderson <theandersonn@gmail.com>
2015-11-18 01:16:36 -0200

    Insere Copyright / assinatura no footer

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <!-- RODAPÉ SITE -->
 <footer>
     FOOTER
+
+    <span>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></span>
 </footer>
 
 <?php wp_footer(); ?>
