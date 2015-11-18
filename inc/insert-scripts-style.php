172d1da34f98f2bba0a4ec6daeb52b8c3abe96d3
Anderson <theandersonn@gmail.com>
2015-11-18 01:28:34 -0200

    Insere chamada fonte dashicons

--- inc/insert-scripts-style.php
+++ inc/insert-scripts-style.php
@@ -11,6 +11,9 @@ function btwp_enqueue_scripts() {
 	wp_register_style('btwp-googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,300,700,900');
 	wp_enqueue_style( 'btwp-googleFonts');
 
+	// Carrega icons da fonte dashicons
+	wp_enqueue_style( 'tema-css', get_stylesheet_uri(), array( 'dashicons' ), '1.0' );		
+
 	// Carrega os estilos do tema
 	wp_enqueue_style('all-css', get_stylesheet_directory_uri() . '/assets/css/all.min.css');
 
@@ -20,3 +23,11 @@ function btwp_enqueue_scripts() {
 	// Carrega scripts JS
 	wp_enqueue_script('scripts-js', get_stylesheet_directory_uri() . '/assets/js/all.min.js');
 }
+
+/* Exemplo de uso - dashicons
+
+.paintbrush-link:before {
+   font-family: "dashicons";
+   content: "\f100";
+}
+*/
