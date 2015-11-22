247c6bcc3cf16799ff651d2bc4579786b8a693b5
Anderson <theandersonn@gmail.com>
2015-11-22 12:09:32 -0200

    Reorganiza css com sass e remove task concatcss

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -4,9 +4,8 @@
 // plugins
 var gulp = require('gulp');
 
-gulp.task('default', ['sass', 'concatcss', 'cssmin', 'concatjs', 'jsmin'], function(){
+gulp.task('default', ['sass', 'cssmin', 'concatjs', 'jsmin'], function(){
     gulp.watch('../assets/sass/**/*.scss', ['sass']);
-    gulp.watch('../assets/css/*.css', ['concatcss']);
     gulp.watch('../assets/css/*.css', ['cssmin']);
 	gulp.watch('../assets/js/*.js', ['concatjs']);
 	gulp.watch('../assets/js/*.js', ['jsmin']);
