6d87538a9b704d0cfd59026ae954b524e99032f9
Anderson <theandersonn@gmail.com>
2015-12-26 18:52:13 -0200

    Insere estrutura e css para paginação

--- category.php
+++ category.php
@@ -16,8 +16,21 @@
     	<p>Nenhum conteúdo encontrado.</p>
     <?php endif; ?>
 
-    <!-- obter paginação -->
-    <?php btwp_pagination(); ?>
+    <!-- OBTER PAGINAÇÃO -->
+    <div class="pagination">
+        <?php wp_paginationn(); ?>
+    </div>
+
+    <!-- <div class="pagination">
+        <ul class="page-numbers">
+            <li><span class="page-numbers current">1</span></li>
+            <li><a href="" class="page-numbers">2</a></li>
+            <li><a href="" class="page-numbers">3</a></li>
+            <li><span class="page-numbers dots">…</span></li>
+            <li><a href="/" class="page-numbers">90</a></li>
+            <li><a href="" class="next page-numbers">Next »</a></li>
+        </ul>
+    </div> -->
 </section>
 
 <?php get_footer(); ?>
