5cbdb5acff26916aa1e99f82de2961b126301b4a
Anderson <theandersonn@gmail.com>
2015-12-29 15:59:13 -0200

    Insere suporte para posts dinâmicos

--- inc/add-theme-suport.php
+++ inc/add-theme-suport.php
@@ -13,3 +13,18 @@ add_image_size('medium', 250, '', true);
 add_image_size('small', 120, '', true);
 add_image_size('custom-size', 700, '', true); 
 
+/*--------------------------------------------------------------
+	HABILITA SUPORTE PARA POSTS DINÂMICOS
+--------------------------------------------------------------*/
+add_theme_support( 'post-formats', array( 
+	'aside', 
+	'image', 
+	'video', 
+	'quote', 
+	'link', 
+	'gallery', 
+	'status', 
+	'audio', 
+	'chat' ) 
+);
+
