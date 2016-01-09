7523426534eaeedfa1bb974b16a6794852534d8f
Anderson <theandersonn@gmail.com>
2016-01-09 18:15:51 -0200

    Update remove junk header

--- inc/remove-junk-header.php
+++ inc/remove-junk-header.php
@@ -13,5 +13,23 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'start_post_rel_link', 10, 0);
 remove_action('wp_head', 'parent_post_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+
+// disable emojis
 remove_action('wp_head', 'print_emoji_detection_script', 7 );
 remove_action('wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
+// disable admin bar
+add_filter('show_admin_bar', '__return_false');
+
+// remove recent comments wp_head css
+add_action( 'widgets_init', 'btwp_remove_recent_comments_style' );
+
+function btwp_remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'  ) );
+}
