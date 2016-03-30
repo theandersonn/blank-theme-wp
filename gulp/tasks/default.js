395a900cdcbd1aa2ef4ed1002378998823357171
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2016-03-30 13:31:17 -0300

    Remove taks javascript da default

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -4,9 +4,7 @@
 // plugins
 var gulp = require('gulp');
 
-gulp.task('default', ['sass', 'cssmin', 'concatjs', 'jsmin'], function(){
+gulp.task('default', ['sass', 'cssmin'], function(){
     gulp.watch('../assets/sass/**/*.scss', ['sass']);
     gulp.watch('../assets/css/*.css', ['cssmin']);
-	gulp.watch('../assets/js/*.js', ['concatjs']);
-	gulp.watch('../assets/js/*.js', ['jsmin']);
 });
