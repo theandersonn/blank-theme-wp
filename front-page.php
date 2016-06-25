5367dc8b1909e92006850981458dda0137df1fc9
Anderson <theandersonn@gmail.com>
2016-06-25 11:54:04 -0300

    Ajustes finos front-page.php

--- front-page.php
+++ front-page.php
@@ -1,10 +1,38 @@
 <?php get_header(); ?>
 
-<!-- CONTEÚDO HOME DO SITE -->
-<main>
+<main class="main">
 	<div class="container">
 		<div class="row">
-			front-page.php		
+			<div class="col-md-12 text-center">
+				<h2>Using Grid</h2>
+			</div>
+
+			<div class="col-md-12 col-example">col 12</div>
+
+			<div class="col-md-4 col-example">col 4</div>	
+			<div class="col-md-4 col-example">col 4</div>	
+			<div class="col-md-4 col-example">col 4</div>
+
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>	
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>			
+			<div class="col-md-1 col-example">col 1</div>						
+
+			<div class="col-md-6 col-example">col 6</div>	
+			<div class="col-md-6 col-example">col 6</div>	
+
+			<div class="col-md-3 col-example">col 3</div>	
+			<div class="col-md-3 col-example">col 3</div>	
+			<div class="col-md-3 col-example">col 3</div>
+			<div class="col-md-3 col-example">col 3</div>
 		</div>
 	</div>
 </main>
