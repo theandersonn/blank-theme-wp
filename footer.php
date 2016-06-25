75217a606b7a631bd3b26ec47f8f5a97cc4896aa
Anderson <theandersonn@gmail.com>
2016-06-25 11:25:35 -0300

    Ajustes finos footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
         <!-- RODAPÉ SITE -->
         <footer>
-			<div class="container">
-				<div class="row">
-                	footer.php
-                	<span>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="<?php echo site_url(); ?>"><?php bloginfo( 'name' ); ?></a></span>					
-				</div>
-			</div>  
+            <div class="container">
+                <div class="row">
+                    footer.php
+                    <span>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="<?php echo site_url(); ?>"><?php bloginfo( 'name' ); ?></a></span>                   
+                </div>
+            </div>  
         </footer>   
         
         <?php wp_footer(); ?>
