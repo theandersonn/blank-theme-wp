28b30938452a57ca97b492009ab91ba9ad5f709f
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2016-07-05 14:50:33 -0300

    Update post-thumbnails

--- inc/add-theme-suport.php
+++ inc/add-theme-suport.php
@@ -9,15 +9,14 @@ add_theme_support( 'title-tag' );
 --------------------------------------------------------------*/
 add_theme_support('post-thumbnails');
 // example: add_image_size( 'name-thump', width, height, true );
-add_image_size('large', 700, '', true);
-add_image_size('medium', 250, '', true);
-add_image_size('small', 120, '', true);
-add_image_size('custom-size', 700, '', true); 
+// add_image_size('large', 700, '', true);
+// add_image_size('medium', 250, '', true);
+// add_image_size('small', 120, '', true);
+// add_image_size('custom-size', 700, '', true); 
 
 
 /*
-THUMBS (PADRÃO)
-
+CONFIGURAR THUMBS NA DASHBOARD DO WORDPRESS
 miniatura 	=>	360*173
 médio 		=>	768*370
 grande 		=> 	1360*655
