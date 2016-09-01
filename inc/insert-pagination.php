7443176acd57dbd34de23c7eb04c0ca29c0241b6
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2016-09-01 19:15:50 -0300

    Update function pagination

--- inc/insert-pagination.php
+++ inc/insert-pagination.php
@@ -1,20 +1,26 @@
 <?php
 /*--------------------------------------------------------------
-	INSERE PAGINAÇÃO
+	INSERT PAGINATION
 --------------------------------------------------------------*/
 function btwp_pagination($pages = '', $range = 9) {
+    
+    $prev_arrow = is_rtl() ? '→' : '←';
+    $next_arrow = is_rtl() ? '←' : '→';
     
     global $wp_query, $wp_rewrite;
     
     $wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
     
     $pagination = array(
-        'base' => @add_query_arg('page','%#%'),
-        'format' => '',
-        'total' => $wp_query->max_num_pages,
-        'current' => $current,
-        'show_all' => true,
-        'type' => 'list'
+        'base'      => @add_query_arg('page','%#%'),
+        'format'    => '',
+        'total'     => $wp_query->max_num_pages,
+        'current'   => $current,
+        'show_all'  => false,
+        'mid_size'  => 2,
+        'type'      => 'list',
+        'prev_text' => $prev_arrow,
+        'next_text' => $next_arrow        
     );
     
     if ( $wp_rewrite->using_permalinks() ) $pagination['base'] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
@@ -22,4 +28,3 @@ function btwp_pagination($pages = '', $range = 9) {
     echo '<div class="pagination">'.paginate_links( $pagination ).'</div>';
 
 }
-
