68d4e0bbb988567f75d37c5820d021d48af5d08f
theandersonn <theandersonn@gmail.com>
2017-07-17 22:32:59 -0300

    Update options thumbs

--- inc/add-theme-suport.php
+++ inc/add-theme-suport.php
@@ -6,9 +6,9 @@ add_theme_support( 'title-tag' );
 // HABILITA IMAGENS DESTACADAS
 add_theme_support('post-thumbnails');
 
-update_option( 'thumbnail_size_h', 0 );
-update_option( 'thumbnail_size_w', 0 );
-update_option( 'medium_size_h', 0 );
-update_option( 'medium_size_w', 0 );
-update_option( 'large_size_h', 0 );
-update_option( 'large_size_w', 0 );
+// update_option( 'thumbnail_size_h', 0 );
+// update_option( 'thumbnail_size_w', 0 );
+// update_option( 'medium_size_h', 0 );
+// update_option( 'medium_size_w', 0 );
+// update_option( 'large_size_h', 0 );
+// update_option( 'large_size_w', 0 );
