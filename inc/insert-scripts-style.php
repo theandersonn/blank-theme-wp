3195828020b6fcdb68d3b1ee7ec04d63b7dceddc
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-09-18 17:32:58 -0300

    Update .editorconfig and helpers js

--- inc/insert-scripts-style.php
+++ inc/insert-scripts-style.php
@@ -9,7 +9,7 @@ function btwp_enqueue_scripts() {
 
 	// Carrega jQuery nativo do WordPress
 	wp_enqueue_script('jquery');
-	
+
 	// Carrega fontes do google
 	wp_register_style('btwp-googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,300,700,900');
 	wp_enqueue_style( 'btwp-googleFonts');
@@ -17,26 +17,26 @@ function btwp_enqueue_scripts() {
 
 	// Carrega os estilos CSS - DESENVOLVIMENTO
 	wp_enqueue_style('all-css', get_stylesheet_directory_uri() . '/static/css/all.css');
-	
+
 	// Carrega os estilos CSS - FINAL
 	//wp_enqueue_style('all-css', get_stylesheet_directory_uri() . '/static/css/all.min.css');
 
 
 	// Carrega scripts JS - DESENVOLVIMENTO
 	wp_enqueue_script('all-js', get_stylesheet_directory_uri() . '/static/js/all.js', array(), null, true);
-	
+
 	// Carrega scripts JS - FINAL
 	//wp_enqueue_script('scripts-js', get_stylesheet_directory_uri() . '/static/js/all.min.js', array(), null, true);
 
     // Internet Explorer specific stylesheet.
     // wp_enqueue_style( 'btwp-ie8', get_stylesheet_directory_uri() . '/static/css/ie8.css', array(), '20170209' );
-    // wp_style_add_data( 'btwp-ie8', 'conditional', 'lte IE 8' );	
+    // wp_style_add_data( 'btwp-ie8', 'conditional', 'lte IE 8' );
 
-	// Internet Explorer HTML5 support 
-    wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/static/js/helpers/html5shiv.js', array(), '3.7.3', false);
+	// Internet Explorer HTML5 support
+    wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/static/js/html5shiv.min.js', array(), '3.7.3', false);
     wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
     // Internet Explorer 8 media query support
-    wp_enqueue_script( 'respond', get_template_directory_uri() . '/static/js/helpers/respond.js', array(), '1.4.2', false);
-    wp_script_add_data( 'respond', 'conditional', 'lt IE 9' );   	
+    wp_enqueue_script( 'respond', get_template_directory_uri() . '/static/js/respond.min.js', array(), '1.4.2', false);
+    wp_script_add_data( 'respond', 'conditional', 'lt IE 9' );
 }
