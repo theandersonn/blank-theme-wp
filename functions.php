53c39a93cf61d6d49d9f1a36dd0c0d063f80a489
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-11-17 18:54:21 -0200

    Update structure includes

--- functions.php
+++ functions.php
@@ -1,24 +1,31 @@
 <?php
 
-// -------------------------------------------
-// CLASSES
-// -------------------------------------------
-require get_template_directory() . '/inc/classes/class-odin-thumbnail-resizer.php';
-require get_template_directory() . '/inc/classes/class-wp-bootstrap-navwalker.php';
+    // prevent file from being loaded directly
+    if ( ! defined( 'ABSPATH' ) ) {
+        die( '-1' );
+    }
 
-// -------------------------------------------
-// FUNCTIONS
-// -------------------------------------------
-require get_template_directory() . '/inc/functions/add-comments-disqus.php';
-require get_template_directory() . '/inc/functions/add-googleanalytics.php';
-require get_template_directory() . '/inc/functions/add-pagination.php';
-require get_template_directory() . '/inc/functions/add-scripts-style.php';
-require get_template_directory() . '/inc/functions/add-thumbnail-admin-columns.php';
-require get_template_directory() . '/inc/functions/add-theme-suport.php';
-require get_template_directory() . '/inc/functions/change-text-size.php';
-require get_template_directory() . '/inc/functions/custom-post-thumbnail.php';
-require get_template_directory() . '/inc/functions/customize-login-admin.php';
-require get_template_directory() . '/inc/functions/register-menus.php';
-require get_template_directory() . '/inc/functions/remove-junk-header.php';
-require get_template_directory() . '/inc/functions/remove-theme-suport.php';
-require get_template_directory() . '/inc/functions/send-mail-post-pending.php';
+    $btwp_template_directory = get_template_directory();
+
+    // -------------------------------------------
+    // classes
+    // -------------------------------------------
+    require $btwp_template_directory . '/includes/classes/class-odin-thumbnail-resizer.php';
+    require $btwp_template_directory . '/includes/classes/class-wp-bootstrap-navwalker.php';
+
+    // -------------------------------------------
+    // functions
+    // -------------------------------------------
+    require $btwp_template_directory . '/includes/functions/add-comments-disqus.php';
+    require $btwp_template_directory . '/includes/functions/add-googleanalytics.php';
+    require $btwp_template_directory . '/includes/functions/add-pagination.php';
+    require $btwp_template_directory . '/includes/functions/add-scripts-style.php';
+    require $btwp_template_directory . '/includes/functions/add-thumbnail-admin-columns.php';
+    require $btwp_template_directory . '/includes/functions/add-theme-suport.php';
+    require $btwp_template_directory . '/includes/functions/change-text-size.php';
+    require $btwp_template_directory . '/includes/functions/custom-post-thumbnail.php';
+    require $btwp_template_directory . '/includes/functions/customize-login-admin.php';
+    require $btwp_template_directory . '/includes/functions/register-menus.php';
+    require $btwp_template_directory . '/includes/functions/remove-junk-header.php';
+    require $btwp_template_directory . '/includes/functions/remove-theme-suport.php';
+    require $btwp_template_directory . '/includes/functions/send-mail-post-pending.php';
