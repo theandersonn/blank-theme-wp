066897c0a97535b03539a4b61b4eb0eb4cbaac9e
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-11-17 09:39:30 -0200

    Remove jquery depency

--- gulp/tasks/concatjs.js
+++ gulp/tasks/concatjs.js
@@ -6,9 +6,6 @@ var concat  = require('gulp-concat');
 
 gulp.task('concatjs', function() {
     return gulp.src([
-      // JQUERY
-      'node_modules/jquery/dist/jquery.js',
-
       // BOOTSTRAP
       'node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
       'node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
