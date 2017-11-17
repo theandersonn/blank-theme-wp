1e7111f4f5bdae0ed8d3e86996f2c3d3a675084b
theandersonn <theandersonn@gmail.com>
2017-11-17 01:19:44 -0200

    Update structure classes and functions

--- functions.php
+++ functions.php
@@ -1,43 +1,51 @@
 <?php
 
-// Habilita funcionalidades suportadas
-require get_template_directory() . '/inc/add-theme-suport.php';
-
-// Limit number of characters
-require get_template_directory() . '/inc/change-text-size.php';
-
-// Custom Post Thumbnail
-require get_template_directory() . '/inc/custom-post-thumbnail.php';
-
-// Customiza o painel de login -> Logo, Título e url
-require get_template_directory() . '/inc/customize-login-admin.php';
-
-// Insere Google Analytics no Footer
-require get_template_directory() . '/inc/insert-googleanalytics.php';
+// -------------------------------------------
+// CLASSES
+// -------------------------------------------
 
 // Odin Thumbnail Resizer
-require get_template_directory() . '/classes/odin-thumbnail-resizer.php';
-
-// Insere Paginação
-require get_template_directory() . '/inc/insert-pagination.php';
-
-// Carrega Scripts e Estilos
-require get_template_directory() . '/inc/insert-scripts-style.php';
-
-// Insere Thumbnail em colunas do admin
-require get_template_directory() . '/inc/insert-thumbnail-admin-columns.php';
+require get_template_directory() . '/inc/classes/odin-thumbnail-resizer.php';
 
 // Insere WP Bootstrap Navwalker
-require get_template_directory() . '/classes/wp-bootstrap-navwalker.php';
+require get_template_directory() . '/inc/classes/wp-bootstrap-navwalker.php';
+
+// -------------------------------------------
+// FUNCTIONS
+// -------------------------------------------
+
+// Habilita funcionalidades suportadas
+require get_template_directory() . '/inc/functions/add-theme-suport.php';
+
+// Limit number of characters
+require get_template_directory() . '/inc/functions/change-text-size.php';
+
+// Custom Post Thumbnail
+require get_template_directory() . '/inc/functions/custom-post-thumbnail.php';
+
+// Customiza o painel de login, Logo, Título e url
+require get_template_directory() . '/inc/functions/customize-login-admin.php';
+
+// Insere Google Analytics no Footer
+require get_template_directory() . '/inc/functions/insert-googleanalytics.php';
+
+// Insere Paginação
+require get_template_directory() . '/inc/functions/insert-pagination.php';
+
+// Carrega Scripts e Estilos
+require get_template_directory() . '/inc/functions/insert-scripts-style.php';
+
+// Insere Thumbnail em colunas do admin
+require get_template_directory() . '/inc/functions/insert-thumbnail-admin-columns.php';
 
 // Cria a action Init e Registra Menus
-require get_template_directory() . '/inc/register-menus.php';
+require get_template_directory() . '/inc/functions/register-menus.php';
 
 // Remove lixo do head
-require get_template_directory() . '/inc/remove-junk-header.php';
+require get_template_directory() . '/inc/functions/remove-junk-header.php';
 
 // Desabilita funcionalidades do tema
-require get_template_directory() . '/inc/remove-theme-suport.php';
+require get_template_directory() . '/inc/functions/remove-theme-suport.php';
 
 // Envia email notificando revisão de post
-require get_template_directory() . '/inc/send-mail-post-pending.php';
+require get_template_directory() . '/inc/functions/send-mail-post-pending.php';
