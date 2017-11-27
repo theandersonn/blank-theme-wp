3b500775e64e3fe3ee7bfea75fde4dbfdd1f885e
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-11-27 11:33:35 -0200

    Added conditional h1 in front-page

--- header.php
+++ header.php
@@ -14,5 +14,18 @@
     <body>
         <!-- ### header ### -->
         <header class="header">
-			header
+            <!-- logotype -->
+            <?php if ( is_front_page() ): ?>
+                <h1 class="header__logo">
+                    <a href="<?php echo site_url(); ?>" title="<?php bloginfo( 'name' ); ?>">
+                        <img src="<?php bloginfo( 'template_url' ); ?>/static/images/logotypes/logo.png" alt="<?php bloginfo( 'name' ); ?>">
+                    </a>
+                </h1>
+            <?php else: ?>
+                <p class="header__logo">
+                    <a href="<?php echo site_url(); ?>" title="<?php bloginfo( 'name' ); ?>">
+                        <img src="<?php bloginfo( 'template_url' ); ?>/static/images/logotypes/logo.png" alt="<?php bloginfo( 'name' ); ?>">
+                    </a>
+                </p>
+            <?php endif; ?>
         </header>
